64e0a3fd27203f105a1fce784ddc803c7b33910d
Swetha Sivasankararaj <110710815+Swetha5021@users.noreply.github.com>
2024-01-31 20:52:01 +0530

    Update EncodeGenerator.py

--- EncodeGenerator.py
+++ EncodeGenerator.py
@@ -6,7 +6,7 @@ import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import db
 from firebase_admin import storage
-   
+     
 cred = credentials.Certificate("serviceAccountKey.json")
 firebase_admin.initialize_app(cred, {
     'databaseURL': "https://faceattendancesystem-77784-default-rtdb.firebaseio.com/",
